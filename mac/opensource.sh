8f943678d895fbba8bc24cab7911a5cf4fc0de03
Maxime Hélias <maximehelias16@gmail.com>
2024-04-04 18:47:09 +0200

    feat: add tools

--- mac/opensource.sh
+++ mac/opensource.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 # Docker Symfony
-git clone https://github.com/dunglas/symfony-docker.git $HOME/www/opensource/symfony-docker
+git clone git@github.com:dunglas/symfony-docker.git $HOME/www/opensource/symfony-docker
 
-# Symfony Flex
-git clone https://github.com/maxhelias/flex $HOME/www/opensource/symfony/flex
-cd $HOME/www/opensource/symfony/flex
-git remote add upstream https://github.com/symfony/flex
+# Flysystem Bundle
+git clone git@github.com:thephpleague/flysystem-bundle.git $HOME/www/opensource/flysystem/flysystem-bundle
 
-cd $HOME
+# PhoneNumber Bundle
+git clone git@github.com:odolbeau/phone-number-bundle.git $HOME/www/opensource/bundle/phone-number-bundle
+
+# OD&B
+git clone git@github.com:odandb/doctrine-ciphersweet-encryption-bundle.git $HOME/www/opensource/odandb/doctrine-ciphersweet-encryption-bundle
